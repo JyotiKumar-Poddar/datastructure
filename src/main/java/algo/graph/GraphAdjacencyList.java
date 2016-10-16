d02e8c5e638faacd1b126b78e022a62ce0aa9919
johnny18445 <jyoti.johney@gmail.com>
2016-10-16 22:43:33 +0530

    Program to demonstrate adjacency list representation of graphs

--- src/main/java/algo/graph/GraphAdjacencyList.java
+++ src/main/java/algo/graph/GraphAdjacencyList.java
@@ -1,21 +1,22 @@
 package algo.graph;
 
 //A structure to represent an adjacency list node
-class AdjListNode{
+class AdjListNode {
 	int dest;
-	AdjListNode next; 
+	AdjListNode next;
 }
-//A structure to represent an adjacency list
-class AdjList{
+
+// A structure to represent an adjacency list
+class AdjList {
 	AdjListNode head;
 }
 
-//A structure to represent a graph. A graph is an array of adjacency lists.
-//Size of array will be V (number of vertices in graph)
-class Graph{
+// A structure to represent a graph. A graph is an array of adjacency lists.
+// Size of array will be V (number of vertices in graph)
+class Graph {
 	int V;
 	AdjList[] array;
-	
+
 	// Method to create a new adjacency list node
 	public AdjListNode newAdjListNode(int dest) {
 		AdjListNode newNode = new AdjListNode();
@@ -23,7 +24,7 @@ class Graph{
 		newNode.next = null;
 		return newNode;
 	}
-	
+
 	// Method that creates a graph of V vertices
 	public Graph createGraph(Graph graph, int V) {
 		graph.V = V;
@@ -31,20 +32,19 @@ class Graph{
 		graph.array = new AdjList[V];
 		// Initialize each adjacency list as empty by making head as NULL
 		int i;
-		for (i = 0; i < V; ++i){
+		for (i = 0; i < V; ++i) {
 			AdjList head = new AdjList();
-			head.head= new AdjListNode();
+			head.head = new AdjListNode();
 			graph.array[i] = head;
 		}
-			
+
 		return graph;
 	}
-	
-	
+
 	// Adds an edge to an undirected graph
 	void addEdge(Graph graph, int src, int dest) {
 		// Add an edge from src to dest. A new node is added to the adjacency
-		// list of src. The node is added at the begining
+		// list of src. The node is added at the beginning
 		AdjListNode newNode = newAdjListNode(dest);
 		newNode.next = graph.array[src].head;
 		graph.array[src].head = newNode;
@@ -53,7 +53,7 @@ class Graph{
 		newNode.next = graph.array[dest].head;
 		graph.array[dest].head = newNode;
 	}
-	
+
 	// Method to print the adjacency list representation of graph
 	void printGraph(Graph graph) {
 		int v;
@@ -69,14 +69,13 @@ class Graph{
 	}
 }
 
-
 public class GraphAdjacencyList {
 	public static void main(String[] args) {
 		// create the graph given in above figure
 		int V = 5;
 		Graph graph = new Graph();
-				
-		graph.createGraph(graph,V);
+
+		graph.createGraph(graph, V);
 		graph.addEdge(graph, 0, 1);
 		graph.addEdge(graph, 0, 4);
 		graph.addEdge(graph, 1, 2);
