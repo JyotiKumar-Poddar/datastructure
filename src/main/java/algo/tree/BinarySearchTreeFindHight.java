19ca029bdcdbc84fa395dd86985398bb401416ef
johnny18445 <jyoti.johney@gmail.com>
2016-11-11 09:56:11 +0530

    Finding height of a binary search tree

---
+++ src/main/java/algo/tree/BinarySearchTreeFindHight.java
@@ -0,0 +1,68 @@
+package algo.tree;
+
+import java.util.Scanner;
+
+
+
+/**
+ * 
+ * @author jyotipoddar
+ *
+ */
+
+class Node{
+    Node left,right;
+    int data;
+    Node(int data){
+        this.data=data;
+        left=right=null;
+    }
+}
+
+public class BinarySearchTreeFindHight {
+
+	public static int getHeight(Node root) {
+		
+		if (null == root) {
+			return -1;
+		} else {
+			int left = getHeight(root.left);
+			int right = getHeight(root.right);
+			if (left > right) {
+				return (left + 1);
+			} else {
+				return (right + 1);
+			}
+		}
+		
+	}
+
+	public static Node insert(Node root, int data) {
+		if (root == null) {
+			return new Node(data);
+		} else {
+			Node cur;
+			if (data <= root.data) {
+				cur = insert(root.left, data);
+				root.left = cur;
+			} else {
+				cur = insert(root.right, data);
+				root.right = cur;
+			}
+			return root;
+		}
+	}
+
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		Node root = null;
+		while (T-- > 0) {
+			int data = sc.nextInt();
+			root = insert(root, data);
+		}
+		sc.close();
+		int height = getHeight(root);
+		System.out.println(height);
+	}
+}
